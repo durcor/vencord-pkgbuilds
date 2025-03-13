6c75f2305b1185e38e39998c6d1f9b0e1b3bfcba
Tyler Kaminski <durcor@disroot.org>
2025-03-12 19:49:10 -0700

    fix: posix-compliant booleans

--- vesktop/vesktop.sh
+++ vesktop/vesktop.sh
@@ -3,7 +3,7 @@
 XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-~/.config}
 
 # Allow users to override command-line options
-if [[ -f $XDG_CONFIG_HOME/vesktop-flags.conf ]]; then
+if [ -f $XDG_CONFIG_HOME/vesktop-flags.conf ]; then
     VESKTOP_USER_FLAGS="$(grep -v '^#' $XDG_CONFIG_HOME/vesktop-flags.conf)"
 fi
 
